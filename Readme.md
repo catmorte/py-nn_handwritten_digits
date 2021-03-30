e080eb527cd917b69e041ff49c5f582f37c95ac4
Raman_Susla <catmorte@gmail.com>
2021-03-30 12:07:11 +0300

    init

---
+++ Readme.md
@@ -0,0 +1,21 @@
+#Handwritten digits NN
+Dataset: https://www.kaggle.com/c/digit-recognizer/code
+##Usage
+
+```python
+from digits_model.digits import predict_digit
+import tensorflow as tf
+
+...
+tf.argmax(predict_digit("Path to image", color_mode="grayscale", is_negative=False), 1)
+```
+
+##Run re-learn
+```
+python ./digits_model/digits.py
+```
+
+##Run unittest
+```
+python -m unittest discover test
+```
---
+++ main.py
@@ -0,0 +1,21 @@
+import unittest
+from digits_model.digits import predict_digit
+import tensorflow as tf
+
+
+class TestModel(unittest.TestCase):
+    def test_images(self):
+        self.assertEqual(tf.argmax(predict_digit(f'./assets/validation_images/0.png', color_mode="grayscale", is_negative=False), 1), 0)
+        self.assertEqual(tf.argmax(predict_digit(f'./assets/validation_images/1.png', color_mode="grayscale", is_negative=False), 1), 1)
+        self.assertEqual(tf.argmax(predict_digit(f'./assets/validation_images/2.png', color_mode="grayscale", is_negative=False), 1), 2)
+        self.assertEqual(tf.argmax(predict_digit(f'./assets/validation_images/3.png', color_mode="grayscale", is_negative=False), 1), 3)
+        self.assertEqual(tf.argmax(predict_digit(f'./assets/validation_images/4.png', color_mode="grayscale", is_negative=False), 1), 4)
+        self.assertEqual(tf.argmax(predict_digit(f'./assets/validation_images/5.png', color_mode="grayscale", is_negative=False), 1), 5)
+        self.assertEqual(tf.argmax(predict_digit(f'./assets/validation_images/6.png', color_mode="grayscale", is_negative=False), 1), 6)
+        self.assertEqual(tf.argmax(predict_digit(f'./assets/validation_images/7.png', color_mode="grayscale", is_negative=False), 1), 7)
+        self.assertEqual(tf.argmax(predict_digit(f'./assets/validation_images/8.png', color_mode="grayscale", is_negative=False), 1), 8)
+        self.assertEqual(tf.argmax(predict_digit(f'./assets/validation_images/9.png', color_mode="grayscale", is_negative=False), 1), 9)
+
+
+if __name__ == '__main__':
+    unittest.main()
