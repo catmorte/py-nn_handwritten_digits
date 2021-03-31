784944c3f7848239b2cfdb6508d0411b8c533adb
Raman_Susla <catmorte@gmail.com>
2021-03-31 17:03:00 +0300

    sandbox

--- digits_model/digits.py
+++ digits_model/digits.py
@@ -41,11 +41,19 @@ else:
     model = krs.models.load_model('./assets/trained_model')
 
 
-def predict_digit(image_path, *, color_mode="grayscale", is_negative=False):
-    for i in range(10):
-        img = krs.preprocessing.image.load_img(image_path, color_mode=color_mode, target_size=(28, 28))
-        data = np.asarray(img, dtype="int32")
-        if not is_negative:
-            data = [255 - x for x in data]
-        data = np.asarray(data).reshape((1, 784))
-        return model.predict(data)
+def predict_digit_from_file(image_path, *, color_mode="grayscale", is_negative=False):
+    img = krs.preprocessing.image.load_img(image_path, color_mode=color_mode, target_size=(28, 28))
+    data = np.asarray(img, dtype="int32")
+    if not is_negative:
+        data = [255 - x for x in data]
+    data = np.asarray(data).reshape((1, 784))
+    return model.predict(data)[0]
+
+
+def predict_digit_from_img(img, *, is_negative=False):
+    img = img.resize(size=(28, 28)).convert('L')
+    data = tf.keras.preprocessing.image.img_to_array(img)
+    if not is_negative:
+        data = [255 - x for x in data]
+    data = np.asarray(data).reshape((1, 784))
+    return model.predict(data)[0]
---
+++ sandbox.py
@@ -0,0 +1,23 @@
+import numpy
+from PIL import Image
+from flask import Flask, jsonify, render_template, request
+from digits_model.digits import predict_digit_from_img
+import tensorflow as tf
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/recognize', methods=['POST'])
+def recognize():
+    img = Image.open(request.files['file'].stream)
+    prediction = predict_digit_from_img(img, is_negative=False)
+    return str([x for x in prediction])
+
+
+if __name__ == "__main__":
+    app.run(port=8080)
